305782ca1d817ae1d90751b113fcb1ae1c4431c9
Usuario1 <Usuario1@DESKTOP-PPN13H4>
2023-03-14 07:48:29 -0500

    Agregar archivos de proyecto.

---
+++ ProyProductosEF/Controllers/ProductController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProyProductosEF.Models;
+using ProyProductosEF.Session;
+
+namespace ProyProductosEF.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: ProductController
+        public IActionResult Index()
+        {
+           return View(listaProductos());
+        }
+        public List<ProductEF> listaProductos()
+        {
+            List<ProductEF> list = SessionHelper.GetObjectFromJson<List<ProductEF>>(HttpContext.Session, "productos");
+            if (list == null)
+            {
+                list = new List<ProductEF>();
+            }
+            return list;
+        }
+        // GET: ProductController/Create
+        [HttpPost]
+        public IActionResult Guardar(ProductEF productef)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    List<ProductEF> list = SessionHelper.GetObjectFromJson<List<ProductEF>>(HttpContext.Session, "productos");
+                    if (list == null)
+                        list = new List<ProductEF>();
+                    list.Add(productef);
+                    SessionHelper.SetObjectAsJson(HttpContext.Session, "productos", list);
+                    return RedirectToAction("Index");
+                }
+                catch (Exception ex)
+                {
+                    TempData["errorCedula"] = ex.Message;
+                    return View("Create", productef);
+                }
+                
+
+            }
+            else
+            {
+                
+                return View("Create", productef);
+            }
+            
+        }
+
+        // POST: ProductController/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+                return View();
+            
+        }
+
+    }
+}
---
+++ ProyProductosEF/Models/ProductEF.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace ProyProductosEF.Models
+{
+    public class ProductEF
+    {
+        private int idProducto;
+        private String nombre;
+        private String categoria;
+        private String detalle;
+
+        public ProductEF() { }
+        public ProductEF(int idProducto, string nombre, string categoria, string detalle)
+        {
+            this.idProducto = idProducto;
+            this.nombre = nombre;
+            this.categoria = categoria;
+            this.detalle = detalle;
+        }
+
+        public int IdProducto { get => idProducto; set => idProducto = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Categoria { get => categoria; set => categoria = value; }
+        public string Detalle { get => detalle; set => detalle = value; }
+    }
+}
